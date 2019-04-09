8eb9353f0f8db249b9bcba0b313a2f6c2c931f70
Ben Kompa <ben.kompa@gmail.com>
2019-04-08 22:23:00 -0500

    implementation of algorithm 5.1

--- GaussianProcessClassifier.py
+++ GaussianProcessClassifier.py
@@ -6,6 +6,7 @@ import jax
 from jax import jit
 import jax.numpy as np
 import jax.scipy as scipy
+import jax.ops
 
 import utils
 
@@ -19,14 +20,15 @@ class GaussianProcessBinaryClassifier:
     """ Binary classification using Gaussian Processes"""
 
     def __init__(self):
-        self.likelihood = lambda x, y: utils.logistic_sigmoid(y*x)
+        self.likelihood = lambda x, y: utils.logistic_sigmoid(x, y)
         self.loglikelihood = lambda x, y: np.log(self.likelihood(x, y))
         self.max_iter = 100
         self.posterior_mode = None
         self.covariance_function = utils.squared_exponential
+        self.covariance_explicit = utils.squared_exponential_explicit
         self.covariance_params = None
 
-    def calculate_posterior_mode(self, train_cov, train_y):
+    def calculate_posterior_mode(self, train_cov, train_y, return_intermeidates=False):
         """Calculate posterior mode using RW Alg 3.1
         Args:
             train_cov: Covariance matrix of training data
@@ -52,6 +54,7 @@ class GaussianProcessBinaryClassifier:
             # calculate lower triangular Cholesky decomposition
             L = scipy.linalg.cholesky(B, lower=True)
             b = np.dot(W, f) + grad_likelihood(f, y)
+            print('Posterior grad likelihood: {}'.format(grad_likelihood(f,y)))
             # calculate a using Cholesky solver
             a = b - np.dot(sqrt_W, scipy.linalg.cho_solve((L, True), np.dot(sqrt_W, np.dot(K, b))))
             # update f
@@ -66,6 +69,9 @@ class GaussianProcessBinaryClassifier:
             previous_log_marginal_likelihood = current_log_marginal_likelihood
 
         self.posterior_mode = f
+
+        if return_intermeidates:
+            return f, current_log_marginal_likelihood, (sqrt_W, L, b, a)
         return f, current_log_marginal_likelihood
 
     def predict_probability(self, train_X, train_y, test_X):
@@ -75,10 +81,11 @@ class GaussianProcessBinaryClassifier:
             train_y: the training targets
             test_X: the test points
         """
-        y = (train_y * 2 - 1)
+        y = train_y * 2. - 1.
         eye = np.eye(y.shape[0])
         train_cov = utils.kernel_matrix(train_X, train_X, self.covariance_function, self.covariance_params)
         f, _ = self.calculate_posterior_mode(train_cov, train_y)
+        print('f posterior: {}'.format(f))
 
         # compute the grad and Hessian of likelihood
         grad_likelihood = jax.vmap(jax.grad(self.loglikelihood), in_axes=0, out_axes=0)
@@ -87,28 +94,76 @@ class GaussianProcessBinaryClassifier:
         # TODO: refactor this into .fit
         W = np.diag(-1. * grad_grad_likelihood(f, y))
         sqrt_W = np.sqrt(W)
+        print('sqrt W: {}'.format(sqrt_W))
         B = eye + np.dot(sqrt_W, np.dot(train_cov, sqrt_W))
         L = scipy.linalg.cholesky(B, lower=True)
 
         K_star = utils.kernel_matrix(train_X, test_X, self.covariance_function, self.covariance_params)
-        f_star = np.dot(np.transpose(K_star), grad_likelihood(f,y))
+        print('likelihood: {}'.format(self.likelihood(f, y)))
+        print('K_star {}'.format(K_star))
+        f_star = np.dot(np.transpose(K_star), grad_likelihood(f, y))
+        print('y: {}'.format(y))
+        print('grad log like: {}'.format(grad_likelihood(f, y)))
+        print('')
+        print('f_star {}'.format(f_star))
         v = scipy.linalg.solve(L, np.dot(sqrt_W, K_star))
 
         K_star_star = utils.kernel_matrix(test_X, test_X, self.covariance_function, self.covariance_params)
-        var_f_star = np.diag(K_star_star), - np.einsum("ij,ij->j", v, v)
+        var_f_star = np.diag(K_star_star) - np.einsum("ij,ij->j", v, v)
 
         # Reference https://github.com/scikit-learn/scikit-learn/blob/7b136e9/sklearn/gaussian_process/gpc.py#L305
-        alpha = 1 / (2 * var_f_star)
+        alpha = 1. / (2. * var_f_star)
         gamma = LAMBDAS * f_star
         integrals = np.sqrt(np.pi / alpha) \
-                    * scipy.special.erf(gamma * np.sqrt(alpha / (alpha + LAMBDAS ** 2))) \
-                    / (2 * np.sqrt(var_f_star * 2 * np.pi))
-        pi_star = (COEFS * integrals).sum(axis=0) + .5 * COEFS.sum()
+                    * scipy.special.erf(gamma * np.sqrt(alpha / (alpha + LAMBDAS ** 2.))) \
+                    / (2. * np.sqrt(var_f_star * 2. * np.pi))
+        pi_star = (COEFS * integrals).sum(axis=0) + 0.5 * COEFS.sum()
 
         return np.vstack((1 - pi_star, pi_star)).T
 
+    def log_marginal_likelihood(self, train_X, train_y, theta=None, return_gradient=False):
+        """Compute the log marginal likelihood and/or grad of log marginal likelihood
+        Based on RW Algorithm 5.1
+        Args:
+            train_X: the training data
+            train_y: the training labels in {0,1}
+            theta: covariance function parameters
+            return_gradient: controls if gradient of log marginal likelihood is returned
+        """
 
+        if theta is None and return_gradient is True:
+            raise ValueError("Graident calculation requires theta!=None")
 
+        # TODO: calculate the training covariance matrix in the .fit function
+        K = utils.kernel_matrix(train_X, train_X, self.covariance_function, theta)
+        y = 2. * train_y - 1.
+        f, current_log_marginal_likelihood, (sqrt_W, L, _, a) = self.calculate_posterior_mode(K, train_y, True)
+
+        if not return_gradient:
+            return current_log_marginal_likelihood
+
+        R = np.dot(sqrt_W, scipy.linalg.cho_solve((L, True), sqrt_W))
+        C = scipy.linalg.solve(L, np.dot(sqrt_W, train_cov))
+
+        # TODO: calculate these functions at construction time
+        grad_loglikelihood = jax.vmap(jax.grad(self.loglikelihood), in_axes=0, out_axes=0)
+        grad3_loglikelihood = jax.vmap(jax.grad(jax.grad(jax.grad(self.loglikelihood))), in_axes=0, out_axes=0)
+
+        s2 = -0.5 * (np.diag(K) - np.einsum('ij, ij -> j', C, C)) * grad3_loglikelihood(f, y)
+
+        d_Z = np.zeros(theta.shape[0])
+
+        # i+2 because arguments 0 and 1 of self.covariance_explicit are data points x and y
+        partial_derivatives_of_kernel = [jax.grad(self.covariance_explicit, argnums=(i+2)) for i in range(theta.shape[0])]
+
+        for idx, param in enumerate(theta):
+            d_K = utils.kernel_matrix(train_X, train_X, partial_derivatives_of_kernel[idx], theta, is_grad=True)
+            s1 = 0.5 * (np.dot(np.transpose(a), np.dot(d_K, a)) - np.dot(np.ravel(np.transpose(R)), np.ravel(d_K)))
+            b = np.dot(d_K, grad_loglikelihood(f, y))
+            s3 = b - np.dot(K, np.dot(R, b))
+            d_Z = jax.ops.index_update(d_Z, idx, s1 + np.dot(np.transpose(s2), s3))
+
+        return current_log_marginal_likelihood, d_Z
 
 
 
--- utils.py
+++ utils.py
@@ -22,45 +22,52 @@ def squared_distance(x1, x2, params=None):
     return -2. * np.dot(x1, x2.T) + np.sum(x2**2, axis=1) + np.sum(x1**2, axis=1)
 
 
-def squared_exponential(x1, x2, params=None):
+def squared_exponential(x1, x2, params):
+    if params is None:
+        # make all the parameters log 1. = 0.
+        return squared_exponential_explicit(x1, x2, 0., 0., 0.)
+    return squared_exponential_explicit(x1, x2, *params)
+
+
+def squared_exponential_explicit(x1, x2, log_amplitude, log_bandwidth, log_length_scale):
     """ The squared exponential function with different scaling parameters
     Args:
-        params: array of length scale parameters
-            params[0] log amplitude
-            params[1] log bandwidth
-            params[2] log length-scale
         x1: numpy array of data
         x2: numpy array of data
+        log_amplitude: the natural log amplitude
+        log_bandwidth: the natural log of bandwidtb
+        log_length_scale: the natural log of length scale
     Returns:
         The squared exponential kernel value of points x1 and x2
     """
-    if params is None:
-        # make all the parameters log 1. = 0.
-        params = np.array([0., 0., 0.])
 
-    x1_scaled = x1 / np.exp(params[1])
-    x2_scaled = x2 / np.exp(params[2])
-    squared_exponential_result = np.exp(params[0]) * np.exp(-0.5 * squared_distance(x1_scaled, x2_scaled) /
-                                                            np.exp(params[2]))
+    x1_scaled = x1 / np.exp(log_length_scale)
+    x2_scaled = x2 / np.exp(log_length_scale)
+    squared_exponential_result = np.exp(log_amplitude) * np.exp(-0.5 * squared_distance(x1_scaled, x2_scaled) /
+                                                            np.exp(log_bandwidth))
 
     return squared_exponential_result
 
 
-def kernel_matrix(x, y, kernel, kernel_params=None):
+def kernel_matrix(x, y, kernel, kernel_params=None, is_grad=False):
     """The kernel matrix for arrays of data x and y
     Args:
         x: a data matrix with points stacked in dim 0
         y: a data matrix with points stacked in dim 0
         kernel: a function such that k[i,j] = kernel(x[i], y[j], kernel_params)
         kernel_params: the parameters for the corresponding kernel function
+        is_grad: determines whether to unpack parameters for jax grad function or not
     Returns: The 2D kernel matrix k[i,j] = kernel(kernel_params, x[i], y[j])
     """
+    if is_grad:
+        return jax.vmap(lambda x_i: jax.vmap(lambda y_j: kernel(x_i, y_j, *kernel_params))(y))(x)
     return jax.vmap(lambda x_i: jax.vmap(lambda y_j: kernel(x_i, y_j, kernel_params))(y))(x)
 
 
-def logistic_sigmoid(x):
+
+def logistic_sigmoid(x, y):
     """The logistic sigmoid function"""
-    return 1. / (1. + np.exp(-1. * x))
+    return 1. / (1. + np.exp(-1. * y * x))
 
 
 
