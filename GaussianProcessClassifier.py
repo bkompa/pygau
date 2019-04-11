83a6f102e9c8daf78f2a9ba64d657e211e914e4e
Ben Kompa <ben.kompa@gmail.com>
2019-04-10 23:46:37 -0400

    implementation of algorithm 5.1

--- GaussianProcessClassifier.py
+++ GaussianProcessClassifier.py
@@ -16,6 +16,7 @@ LAMBDAS = np.array([0.41, 0.4, 0.37, 0.44, 0.39])[:, np.newaxis]
 COEFS = np.array([-1854.8214151, 3516.89893646, 221.29346712,
                   128.12323805, -2010.49422654])[:, np.newaxis]
 
+
 class GaussianProcessBinaryClassifier:
     """ Binary classification using Gaussian Processes"""
 
@@ -54,7 +55,6 @@ class GaussianProcessBinaryClassifier:
             # calculate lower triangular Cholesky decomposition
             L = scipy.linalg.cholesky(B, lower=True)
             b = np.dot(W, f) + grad_likelihood(f, y)
-            print('Posterior grad likelihood: {}'.format(grad_likelihood(f,y)))
             # calculate a using Cholesky solver
             a = b - np.dot(sqrt_W, scipy.linalg.cho_solve((L, True), np.dot(sqrt_W, np.dot(K, b))))
             # update f
@@ -85,7 +85,6 @@ class GaussianProcessBinaryClassifier:
         eye = np.eye(y.shape[0])
         train_cov = utils.kernel_matrix(train_X, train_X, self.covariance_function, self.covariance_params)
         f, _ = self.calculate_posterior_mode(train_cov, train_y)
-        print('f posterior: {}'.format(f))
 
         # compute the grad and Hessian of likelihood
         grad_likelihood = jax.vmap(jax.grad(self.loglikelihood), in_axes=0, out_axes=0)
@@ -94,18 +93,11 @@ class GaussianProcessBinaryClassifier:
         # TODO: refactor this into .fit
         W = np.diag(-1. * grad_grad_likelihood(f, y))
         sqrt_W = np.sqrt(W)
-        print('sqrt W: {}'.format(sqrt_W))
         B = eye + np.dot(sqrt_W, np.dot(train_cov, sqrt_W))
         L = scipy.linalg.cholesky(B, lower=True)
 
         K_star = utils.kernel_matrix(train_X, test_X, self.covariance_function, self.covariance_params)
-        print('likelihood: {}'.format(self.likelihood(f, y)))
-        print('K_star {}'.format(K_star))
         f_star = np.dot(np.transpose(K_star), grad_likelihood(f, y))
-        print('y: {}'.format(y))
-        print('grad log like: {}'.format(grad_likelihood(f, y)))
-        print('')
-        print('f_star {}'.format(f_star))
         v = scipy.linalg.solve(L, np.dot(sqrt_W, K_star))
 
         K_star_star = utils.kernel_matrix(test_X, test_X, self.covariance_function, self.covariance_params)
@@ -134,7 +126,7 @@ class GaussianProcessBinaryClassifier:
         if theta is None and return_gradient is True:
             raise ValueError("Graident calculation requires theta!=None")
 
-        # TODO: calculate the training covariance matrix in the .fit function
+        # TODO: calculate the training covariance matrix in the .fit function #can't reuse cause theta might change
         K = utils.kernel_matrix(train_X, train_X, self.covariance_function, theta)
         y = 2. * train_y - 1.
         f, current_log_marginal_likelihood, (sqrt_W, L, _, a) = self.calculate_posterior_mode(K, train_y, True)
@@ -143,7 +135,7 @@ class GaussianProcessBinaryClassifier:
             return current_log_marginal_likelihood
 
         R = np.dot(sqrt_W, scipy.linalg.cho_solve((L, True), sqrt_W))
-        C = scipy.linalg.solve(L, np.dot(sqrt_W, train_cov))
+        C = scipy.linalg.solve(L, np.dot(sqrt_W, K))
 
         # TODO: calculate these functions at construction time
         grad_loglikelihood = jax.vmap(jax.grad(self.loglikelihood), in_axes=0, out_axes=0)
@@ -164,11 +156,3 @@ class GaussianProcessBinaryClassifier:
             d_Z = jax.ops.index_update(d_Z, idx, s1 + np.dot(np.transpose(s2), s3))
 
         return current_log_marginal_likelihood, d_Z
-
-
-
-
-
-
-
-
